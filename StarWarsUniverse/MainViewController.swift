400d2db35eceed850d0b46f953ea0ed2aebba88a
Viacheslav Markov <markvyach83@gmail.com>
2022-12-26 22:15:37 +0200

    removed storyboards

--- StarWarsUniverse/MainViewController.swift
+++ StarWarsUniverse/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  StarWarsUniverse
 //
 //  Created by Viacheslav Markov on 26.12.2022.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 }
